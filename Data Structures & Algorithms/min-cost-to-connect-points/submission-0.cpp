afec2cf35165d92c8ada6c1236dd722564d95cbe
Kenaz <66073275+Kencel@users.noreply.github.com>
2026-09-05 00:45:47 +0800

    Add: min-cost-to-connect-points - submission-0

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-0.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> parent;
+    vector<int> comp_size;
+    
+    int find(int u){ return parent[u] == u? u : parent[u] = find(parent[u]); }
+    
+    void unite(int u, int v){
+        int par_u = find(u), par_v = find(v);
+        if(par_v != par_u) {
+            if(comp_size[par_v] < comp_size[par_u]) swap(par_u, par_v);
+            parent[par_u] = par_v;
+            comp_size[par_v] += comp_size[par_u];
+        }
+    }
+
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        int n = points.size();
+        parent.resize(n);
+        for(int i=0; i < n; i++) parent[i] = i;
+        comp_size.resize(n, 1);
+        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>>> pq;
+        for(int i=0; i < n; i++){
+            for(int j=i + 1; j < n; j++){
+                pq.push({abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1]), i, j});
+            }
+        }
+        int ans = 0;
+        while(!pq.empty()){
+            int dist, u, v;
+            tie(dist, u, v) = pq.top();
+            pq.pop();
+            if(find(u) == find(v)) continue;
+            ans += dist;
+            unite(u, v);
+        }
+        return ans;
+    }
+};
